d0c34e0a7456cf10fb0f7a070b6342174a64429a
Cerdtruir <ghoornabeel@gmail.com>
2022-04-23 18:43:41 +0200

    Deconstruct props and verify proptypes

--- src/components/book.js
+++ src/components/book.js
@@ -1,18 +1,25 @@
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { removeBook } from '../redux/books/books';
 
-const Book = (props) => {
+const Book = ({ title, author, id }) => {
   const dispatch = useDispatch();
 
   return (
     <div>
-      <h3>{props.title}</h3>
-      <h4>{props.author}</h4>
-      <button onClick={() => dispatch(removeBook(props.id))} type="button">
+      <h3>{title}</h3>
+      <h4>{author}</h4>
+      <button onClick={() => dispatch(removeBook(id))} type="button">
         Remove
       </button>
     </div>
   );
+};
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
