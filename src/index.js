8a7b9f6420b0c32a848579b250e6462e8d133ae3
Cerdtruir <ghoornabeel@gmail.com>
2022-04-27 23:40:27 +0200

    Change name of store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import configureStore from './redux/configureStore';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={configureStore}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
